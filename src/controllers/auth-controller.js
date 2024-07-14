340908fd68393bb49354ed55afd3fdc0b57c5d10
codemitrayt <codemitrayt@gmail.com>
2024-07-14 23:08:19 +0530

    feat: add auth controller

---
+++ src/controllers/auth-controller.js
@@ -0,0 +1,41 @@
+const Joi = require("joi");
+
+class AuthController {
+  signUp(req, res) {
+    const { fullName, email, password, confirmPassword } = req.body;
+
+    const signUpSchema = Joi.object({
+      fullName: Joi.string().min(3).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6).required(),
+      confirmPassword: Joi.ref("password"),
+    });
+
+    const { error } = signUpSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        message: "All fields required",
+        error: error.details[0].message,
+      });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        message: "All fields required",
+        error: "Password does not match",
+      });
+    }
+
+    /*
+        1. front data (data modeling)
+        2. validate front data 
+        3. check email already registered
+        4. Otp send to user email (logic)
+        5. return response
+    */
+
+    return res.json({ message: "Otp sent successfully" });
+  }
+}
+
+module.exports = AuthController;
