5a023b98b54fce8433587bac52768a1b6dcfdbe0
codemitrayt <codemitrayt@gmail.com>
2024-07-19 20:31:34 +0530

    add async wrapper to sign-up route

--- src/router.js
+++ src/router.js
@@ -1,13 +1,18 @@
 const router = require("express").Router();
+
+const asyncWrapper = require("./utils/async-wrapper");
+const TokenService = require("./services/token-service");
 const AuthController = require("./controllers/auth-controller");
 const AuthService = require("./services/auth-service");
 const UserModel = require("./model/user-model");
 
+const tokenService = new TokenService();
 const authService = new AuthService(UserModel);
-const authController = new AuthController(authService);
+const authController = new AuthController(authService, tokenService);
 
-router.post("/auth/sign-up", (req, res, next) =>
-  authController.signUp(req, res, next)
+router.post(
+  "/auth/sign-up",
+  asyncWrapper((req, res, next) => authController.signUp(req, res, next))
 );
 router.post("/auth/login", (req, res, next) =>
   authController.login(req, res, next)
